047dbe385d5cd4f072e5cb214071d908ff5cb14c
vuonga1103 <vuonga1103@yahoo.com>
2020-12-14 16:30:28 -0500

    .

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://hopeful-minsky-7f4b3b.netlify.app/'
+    origins 'https://hopeful-minsky-7f4b3b.netlify.app'
 
     resource '*',
       headers: :any,
