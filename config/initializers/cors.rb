585e1d42007b9ac1240a44f621d9d9065c79e09a
vuonga1103 <vuonga1103@yahoo.com>
2020-12-14 23:47:34 -0500

    changed cors origin, yes again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://laughing-visvesvaraya-16baf3.netlify.app'
 
     resource '*',
       headers: :any,
